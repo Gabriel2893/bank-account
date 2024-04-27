0b370abe26de8698edfc990afe614afb68558bb3
Gabriel Alves <gabrielalves2892@gmail.com>
2024-04-27 19:15:30 -0300

    initial commit

---
+++ ContaBanco.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.Scanner;
+
+public class ContaBanco {
+    //Atributos
+    public int numeroDaConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+    //Metodos do sistema
+
+    public void abrirConta(){
+
+    }
+
+    public void fecharConta(){
+
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void sacar(){
+
+    }
+
+    public void pagarMensalidade(){
+
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
