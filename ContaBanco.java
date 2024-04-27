a6a3206f54a6daaab5cfd98c14031ee79d76c012
Gabriel Alves <gabrielalves2892@gmail.com>
2024-04-27 19:34:31 -0300

    metodo construtor e getters and setters criados

--- ContaBanco.java
+++ ContaBanco.java
@@ -8,6 +8,7 @@ public class ContaBanco {
     private String dono;
     private float saldo;
     private boolean status;
+
     //Metodos do sistema
 
     public void abrirConta(){
@@ -30,6 +31,49 @@ public class ContaBanco {
     public void pagarMensalidade(){
 
     }
+    //Método construtor
+    public ContaBanco(){
 
+    }
 
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+    //Métodos getters and setters
+
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
