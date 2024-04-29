de5ca8e2243551f8fa896939a630eceef787437b
Gabriel Alves <146888502+Gabriel2893@users.noreply.github.com>
2024-04-29 15:54:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         BankAccount p1 = new BankAccount();
         p1.setAccountNumber(1);
-        p1.setOwner("craudio");
+        p1.setOwner("Oscar");
         p1.openAccount("Current account");
         p1.currentStatus();
 
